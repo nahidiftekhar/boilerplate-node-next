fc243ff558d4347e5104f4aaa4541cbc16305f2a
Nahid Iftekhar <100844948+nahidiftekhar@users.noreply.github.com>
2023-04-01 18:00:16 +0600

    Config template

--- backend/configs/db.config.js
+++ backend/configs/db.config.js
@@ -1,11 +1,11 @@
 const dbConfig = {
     host: "localhost",
-    port: 5432,
-    user: "smarttaps",
-    password: "beautifulcards",
-    database: "smarttaps_db",
+    port: 5432, //or whichever port db is running
+    user: "db_username",
+    password: "db_password",
+    database: "db_schema",
 
-    dialect: "postgres",
+    dialect: "postgres", //or other dialect as per sequelize
     pool: {
       max: 5,
       min: 0,
