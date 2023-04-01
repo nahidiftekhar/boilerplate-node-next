e9a8ca1880e87915ee01717255d63399a1918484
Nahid Iftekhar <nahid.iftekhar13@gmail.com>
2023-04-01 17:25:56 +0600

    config templates

--- backend/configs/mail.config.js
+++ backend/configs/mail.config.js
@@ -1,12 +1,9 @@
 const mailConfig = {
-    EMAIL_USERNAME: 'noreply@smarttaps.co',
-    // EMAIL_USERNAME: 'jogfol@bdlancelimited.com',
-    EMAIL_PASSWORD: '4jnUq6f)ex;u',
-    // EMAIL_PASSWORD: 'rjD9%iSH#fRL',
+    EMAIL_USERNAME: 'email_id',
+    EMAIL_PASSWORD: 'password',
     EMAIL_PORT: '465',
-    EMAIL_HOST: 'mail.smarttaps.co',
-    // EMAIL_HOST: 'server107.web-hosting.com',
-    SERVER_NAME: 'www.smarttaps.co',
+    EMAIL_HOST: 'host',
+    SERVER_NAME: 'servername',
     EMAIL_TO: 'nahid.iftekhar@codemarshal.com',
     DEFAULT_TEXT: 'This mail is generated from smart taps'
     }
