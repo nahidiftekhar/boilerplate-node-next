f2b58c392a3ed0beea75e41e0c65a45f5eda5ed1
Nahid Iftekhar <nahid.iftekhar13@gmail.com>
2023-04-01 17:50:39 +0600

    remove conf files

--- backend/configs/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-    PORT: 3420,
-    listPerPage: 10,
-    WHITELISTED_DOMAINS:  "http://localhost:3421, http://localhost, http://localhost:3420, https://www.bdlancelimited.com, http://10.10.10.102:3421",
-    // WHITELISTED_DOMAINS: "*",
-    APP_URL: "localhost:3421"
-};
-module.exports = {config};
